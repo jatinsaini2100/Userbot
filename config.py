2052ca6e04796f5a2b74be1b28421132ae480d03
jatinsaini2100 <128668150+jatinsaini2100@users.noreply.github.com>
2024-01-21 10:07:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "29250051")) #optional
+API_HASH = getenv("API_HASH", "5a8e5c861695595f38ec59b6e0a0f6cc") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6155567796").split()))
+OWNER_ID = int(getenv("5366891026"))
+MONGO_URL = getenv("mongodb+srv://Jatin:Jatin@cluster0.zdkvzab.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "6444309600:AAGM2jtq4LRkNrN7xv_m_c7GAxe83F9pUuo")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://te.legra.ph/file/d735867c2930509c1283b.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/TEAMxLIGIT/Ligit-Userbot")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQHH73AARZJ4Lst76qYuhbYvHGSTK2p_YpM-4C-Q2Xi4giBlVSgtRlj1IpE8xMPooL6YQpepPP0PV-ES7fIGSu9Bc7kyNCxkFm8Yqdff1F7yQprzEPgdG3sZgJBT-lkWtoliAdB6EDk44MIbeDcpjOBCJIvi02mNHevxFNVLiLQpGoOJsbkuQqG8fqgkOxz3wIXMcbGVstwWlymVPA-bTwPur3Kow1JMVJPq4pVgxpVTtsVBFXJD-PztCIB9cH1GrjM9j_KBDAwWsgmlmgj6_Jav3Vsa0trNWniXj7JnxaSl3CmQtGmh0x1WecANAP9aBOX3oc8VQSH6BceX3fSGPc6yWHIFqQAAAAFu5oK0AA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
